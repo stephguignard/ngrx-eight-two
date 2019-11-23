965d2f5132a17320b27b43960fb2056f8aec4555
stephane <gui_steph@yahoo.fr>
2019-11-23 18:03:47 +0100

    Add bootstrap grid and utilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToDoComponent } from './to-do/to-do.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ToDoComponent } from "./to-do/to-do.component";
 
-import { StoreModule } from '@ngrx/store';
-import { ToDoReducer } from './todo.reducer';
-import { TodoEffects } from './todo.effects';
-import { EffectsModule } from '@ngrx/effects';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from "@ngrx/store";
+import { ToDoReducer } from "./todo.reducer";
+import { TodoEffects } from "./todo.effects";
+import { EffectsModule } from "@ngrx/effects";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent, ToDoComponent],
@@ -32,6 +33,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatCheckboxModule,
     MatMenuModule,
+    MatButtonModule,
     MatButtonToggleModule,
     MatIconModule
   ],
